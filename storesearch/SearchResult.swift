ee7c9b7a152367f9919e91ac09735f198ebe53ae
한석희 <7616tjrgml@hanmail.net>
2020-12-25 19:27:13 +0900

    tableview base developed

---
+++ storesearch/SearchResult.swift
@@ -0,0 +1,19 @@
+//
+//  SearchResult.swift
+//  storesearch
+//
+//  Created by 한석희 on 12/25/20.
+//
+
+import Foundation
+
+class SearchResult {
+    
+    var name = ""
+    var artistName = ""
+    
+}
+
+
+
+
--- storesearch/SearchViewController.swift
+++ storesearch/SearchViewController.swift
@@ -8,15 +8,22 @@
 import UIKit
 
 class SearchViewController: UIViewController {
+    
     //MARK: - Ins Vars
+        //
     @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var tableView: UITableView!
-    var searchResults = [String]()
+        //
+    var searchResults = [SearchResult]()
+    var hasSearched = false
+    //
     
     //MARK:- View did load
     override func viewDidLoad() {
         super.viewDidLoad()
+        //
         self.tableView.contentInset = UIEdgeInsets(top: 50, left: 0, bottom: 0, right: 0)
+        self.searchBar.backgroundColor = UIColor.blue // 경계선, 배경까지 변형
         // delegate search bar  - done in SB
     }
 }
@@ -25,17 +32,25 @@ class SearchViewController: UIViewController {
 extension SearchViewController : UISearchBarDelegate {
     // 1.
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        //
+        hasSearched = true
+        //
         searchBar.resignFirstResponder()
-        print( "The search text is : \(searchBar.text!)" )
-        searchResults = []
-        for i in 0...2{
-            searchResults.append(
-                String( format: " Fake result %d for '%@' ", i, searchBar.text! )// %d, %f, %@ : int, float, object(class, struct) -> format specifier
-            )
+        searchResults = [ ]
+        //
+        if searchBar.text != "justin bieber" {
+            for i in 0...2{
+                let searchResult = SearchResult()
+                searchResult.name = String(format: "Fake Result %d for", i)
+                searchResult.artistName = searchBar.text!
+                searchResults.append(searchResult)
+            }
         }
         self.tableView.reloadData()
     }
-    // 2.
+        
+    // 2. SearchBarDelegate : UIBarPositioning(protocol)
+        // In default, the delegate returns UIBarPosition.top
     func position(for bar: UIBarPositioning) -> UIBarPosition {
         return .topAttached
         // allowing their background content to show through the status bar
@@ -44,24 +59,52 @@ extension SearchViewController : UISearchBarDelegate {
 }
 
 //MARK: - TableView Related Delegate
-extension SearchViewController : UITableViewDelegate, UITableViewDataSource {
-    
+    extension SearchViewController : UITableViewDelegate, UITableViewDataSource {
     // MARK: - DataSource
         // 1.
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        searchResults.count
+        guard hasSearched == true else {
+            return 0
+        }
+        guard searchResults.count != 0 else{
+            return 1
+        }
+        return searchResults.count
     }
         // 2.
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cellID = "SearchResultCell"
-        var cell : UITableViewCell! = tableView.dequeueReusableCell( withIdentifier: cellID )
+        var cell : UITableViewCell! = self.tableView.dequeueReusableCell( withIdentifier: cellID )
         // using the class or nib file you previously registered, If not return nil
         if cell == nil {
-            cell = UITableViewCell(style: .default, reuseIdentifier: cellID )
+            cell = UITableViewCell(style: .subtitle, reuseIdentifier: cellID )
         }
-        cell.textLabel!.text = searchResults[indexPath.row]
+        //
+        if searchResults.count == 0 {
+            cell.textLabel!.text = "(Nothing Found)"
+            cell.textLabel?.textColor = .lightGray
+            cell.detailTextLabel!.text = ""
+            return cell
+        }
+        let searchResult = searchResults[indexPath.row]
+        cell.textLabel!.text = searchResult.name
+        cell.textLabel?.textColor = .black
+        cell.detailTextLabel!.text = searchResult.artistName
+        //
         return cell
     }
-    //
+        //
+        func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
+            if hasSearched == false || searchResults.count == 0 {
+                return nil
+            }
+            else {
+                return indexPath
+            }
+        }
+        //
+        func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+            tableView.deselectRow(at: indexPath, animated: true)
+        }
     
 }
